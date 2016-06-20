a1082832509df15b9c7ac40308e544a0adb661bc
Darya Veretennikova <darya.veretennikova@kelnik.ru>
2016-06-20 11:56:30 +0300

    end week 2

--- client/main.js
+++ client/main.js
@@ -7,7 +7,7 @@ Images = new Mongo.Collection('images');
 
 Template.images.helpers({
   images:
-    Images.find({}, {sort:{rating:-1}})
+    Images.find({}, {sort:{createdOn: -1, rating:-1}})
 });
 
 Template.images.events({
@@ -26,5 +26,28 @@ Template.images.events({
     var rating = $(e.currentTarget).data('userrating');
     var image_id = this.id;
     Images.update({'_id': image_id}, {$set: {'rating': rating}});
+  },
+  'click .js-show-image-form': function(e) {
+    $('#image_add_form').modal('show');
   }
+});
+
+Template.image_add_form.events({
+    'submit .js-add-image': function(e) {
+        var img_src;
+        var img_alt;
+
+        img_src = event.target.img_src.value;
+        img_alt = event.target.img_alt.value;
+
+        Images.insert({
+            img_src: img_src,
+            img_alt: img_alt,
+            createdOn: new Date()
+        });
+
+        $('#image_add_form').modal('hide');
+
+        return false;
+    }
 });
